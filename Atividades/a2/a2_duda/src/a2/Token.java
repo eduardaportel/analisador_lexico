90ddb9255160a4557c47b21eb0162795e359298f
Eduarda Portel <eduarda.portel4@gmail.com>
2025-03-28 12:13:55 -0300

    feat: create branch  'a2_duda' for alternative exercise 2 implementation

---
+++ Atividades/a2/a2_duda/src/a2/Token.java
@@ -0,0 +1,15 @@
+package exemplo1;
+
+// Classe mais geral de token
+public class Token {
+    public final Tag tag;
+
+    public Token(Tag t) {
+        tag = t;
+    }
+
+    @Override
+    public String toString() {
+        return "Token: <" + tag + ">";
+    }
+}
